9fa66c7e1b2aec81220a9cb59b8d3ba8870274cf
ngbusca <ngbusca@in2p3.fr>
2016-12-03 18:50:39 +0100

    simplyfied constants.py

--- py/pylya/constants.py
+++ py/pylya/constants.py
@@ -5,34 +5,23 @@ lya=1215.67 ## angstrom
 
 deg = sp.pi/180.
 
-nside = 256
-nest=False
 
+class cosmo:
+    ## ignore Orad and neutrinos
+    nbins=10000
+    zmax=10.
+    dz = zmax/nbins
+    z=sp.array(range(nbins))*dz
 
-z0 = 2.25
-alpha=3.8
+    def __init__(self,Om):
+        H0 = 100. ## km/s/Mpc
+        hubble = H0*sp.sqrt(Om*(1+z)**3+1-Om)
+        c = 299792.4583
 
-nbins=10000
-zmax=10.
-dz = zmax/nbins
-z=sp.array(range(nbins))*dz
+        chi=sp.zeros(nbins)
+        for i in range(1,nbins):
+            chi[i]=chi[i-1]+c*(1./hubble[i-1]+1/hubble[i])/2.*dz
 
-Om = 0.315
-H0=100.
-hubble = H0*sp.sqrt(Om*(1+z)**3+1-Om)
-c = 299792.4583
+        self.r_comoving = interpolate.interp1d(z,chi)
+        self.hubble = interpolate.interp1d(z,hubble)
 
-chi=sp.zeros(nbins)
-for i in range(1,nbins):
-    chi[i]=chi[i-1]+c*(1./hubble[i-1]+1/hubble[i])/2.*dz
-
-r_comoving = interpolate.interp1d(z,chi)
-chi = interpolate.interp1d(z,chi)
-hubble = interpolate.interp1d(z,hubble)
-
-cross_rpar_min = -200.
-cross_rpar_max = 200.
-cross_npar = 100
-
-rper_max = 200.
-nper = 50
